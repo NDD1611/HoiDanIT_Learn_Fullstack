dfbda1440dae72ea65a625857c4d20d87c5ff950
DinhNguyen <nguyendinhdinh16112000@gmail.com>
2022-06-23 15:08:20 +0700

    commit vd #23

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -1,17 +1,21 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Nguyen',
+      address: 'VN',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
