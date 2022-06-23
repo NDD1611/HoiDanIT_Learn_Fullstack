c7a7c145edd4576d0605858d9780569df7e8e7ac
DinhNguyen <nguyendinhdinh16112000@gmail.com>
2022-06-23 15:25:43 +0700

    commit vd #24 3:25 23/06/2022

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -4,11 +4,10 @@ import db from '../models/index.js'
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
-        console.log(data)
         return res.render("homepage.ejs", {
             data: JSON.stringify(data)
         });
-    } catch (e){
+    } catch (e) {
         console.log(e);
     }
 }
